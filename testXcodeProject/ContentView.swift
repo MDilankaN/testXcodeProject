ec4dd80cf883a9ef6f8e86013ca488d027266213
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2024-12-17 21:27:05 +0530

    init xcode project

--- testXcodeProject/ContentView.swift
+++ testXcodeProject/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack
+        {
+            Color.black.ignoresSafeArea()
+            VStack{
+                Image("imageAbstract").resizable().cornerRadius(20).aspectRatio(contentMode: .fit).padding(.all)
+                Text("Abstract Image").font(.title).fontWeight(.semibold).foregroundColor(.white)
+            }
         }
-        .padding()
+
+
     }
 }
 
