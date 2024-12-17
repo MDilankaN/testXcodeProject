100f56ada7a53c2d2d9d151c2ff0d4fabce214cd
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2024-12-17 19:08:12 +0530

    Initial Commit

---
+++ testXcodeProject/testXcodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testXcodeProjectApp.swift
+//  testXcodeProject
+//
+//  Created by Menaka Dilanka on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct testXcodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testXcodeProjectTests/testXcodeProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  testXcodeProjectTests.swift
+//  testXcodeProjectTests
+//
+//  Created by Menaka Dilanka on 2024-12-17.
+//
+
+import Testing
+@testable import testXcodeProject
+
+struct testXcodeProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
